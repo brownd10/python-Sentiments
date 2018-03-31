f73b0af1190d54f15777826fa03c833cd323862d
brownd10 <brown.derell@gmail.com>
2018-03-31 12:10:12 -0400

    updates to config

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'gyFT23Gaocfy33trnvWJZscIZ'
+consumer_secret = '9x1TAxWYgCFIwvRdSOgMYAMD2yEwCmXChuJ5k1v6amcuwWqCV7'
+access_token = '977549487819444227-p3vAeRpDaye1KTVmql96NzX9o6dmcSF'
+access_token_secret = 'sfHbrRIK9NFNxrmhkpbrcp0Q2anGwQp8LAsRUJqSRU92l'
